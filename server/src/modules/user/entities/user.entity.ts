28214c0c541f1e13033db1ca59e3790c0757beb6
hchaejeong <hchaejeong@kaist.ac.kr>
2024-01-05 13:52:42 +0900

    first

---
+++ server/src/modules/user/entities/user.entity.ts
@@ -0,0 +1,54 @@
+import { Exclude, Expose } from "class-transformer";
+import { SeriesEntity } from "src/modules/series/entities/series.entity";
+import { BaseEntity, Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({
+    name: 'user',
+})
+export class UserEntity extends BaseEntity {
+    @PrimaryGeneratedColumn('uuid')
+    @Expose()
+    id: string;
+
+    @Column({
+        type: 'varchar',
+        length: 50,
+    })
+    @Expose()
+    name: string;
+
+    @Column({
+        type: 'varchar',
+        length: 255,
+        unique: true,
+    })
+    @Expose()
+    email: string;
+
+    @Column({
+        type: 'varchar'
+    })
+    @Exclude({
+        toPlainOnly: true,
+    })
+    password: string;
+
+    @Column({
+        default: false,
+    })
+    @Expose()
+    emailVerified: boolean;
+
+    @Column({
+        type: 'varchar',
+        length: 200,
+        nullable: true,
+    })
+    @Expose()
+    profileIconUrl: string;
+
+    @OneToMany(() => SeriesEntity, (series) => series.user)
+    @Exclude({ toPlainOnly: true })
+    series: SeriesEntity[] | null;
+
+}
