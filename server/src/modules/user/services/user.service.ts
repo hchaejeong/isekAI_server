b7609d0edefd01bb3d7d4a045ba6e99798d28860
hchaejeong <hchaejeong@kaist.ac.kr>
2024-01-06 17:47:27 +0900

    유저 모듈 수정

--- server/src/modules/user/services/user.service.ts
+++ server/src/modules/user/services/user.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { UserRepository } from '../repository/user.repository';
+import { UserEntity } from '../entities/user.entity';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(private userRepository: UserRepository) {}
+
+    public async updateProfile(args: { id: string; name: string; profileIconUrl?: string }): Promise<string> {
+        const { id, name, profileIconUrl } = args;
+    
+        await this.userRepository.update(id, {
+          name,
+          profileIconUrl,
+        });
+    
+        return 'your profile has been updated';
+      }
+}
