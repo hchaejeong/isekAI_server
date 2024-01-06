94f0927480a661e58cbf26e0621f984f906fadce
hchaejeong <hchaejeong@kaist.ac.kr>
2024-01-06 17:45:24 +0900

    google login 연동
    - 처음으로 로그인한 유저는 new user로 리턴
    - 기존 유저는 returning user로 리턴

---
+++ server/src/modules/oauth/strategy/google.strategy.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, Profile, VerifyCallback } from 'passport-google-oauth20';
+import { OauthService } from '../oauth.service';
+
+@Injectable()
+export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
+  constructor(private readonly oauthService: OauthService) {
+    super({
+      clientID: process.env.OAUTH_GOOGLE_ID,
+      clientSecret: process.env.OAUTH_GOOGLE_SECRET,
+      callbackURL: process.env.OAUTH_GOOGLE_REDIRECT,
+      scope: ['email', 'profile'],
+    });
+  }
+
+  async validate(accessToken: string, refreshToken: string, profile: any, done: VerifyCallback): Promise<any> {
+    const { name, emails, photos } = profile;
+    const user = {
+      email: emails[0].value,
+      firstName: name.givenName,
+      lastName: name.familyName,
+      profileIcon: photos[0].value,
+      accessToken,
+    };
+    done(null, user);
+  }
+}
